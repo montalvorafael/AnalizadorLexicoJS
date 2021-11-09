5555f9ec6d49289ea483f07399aea1debc8d0e3e
Rafael Montalvo <rafamont@espol.edu.ec>
2021-11-09 02:26:19 -0500

    palabras reservadas

--- main.py
+++ main.py
@@ -6,3 +6,35 @@
 
 
 import ply.lex as lex
+
+#Palabras reservadas JS
+reserved = {
+    'class' : 'CLASS',
+    'var' : 'VAR',
+    'let' : 'LET',
+    'if' : 'IF',
+    'else' : 'ELSE',
+    'while' : 'WHILE',
+    'switch' : 'SWITCH',
+    'for' : 'FOR',
+    'return' : 'RETURN',
+    'null' : 'NULL',
+    'catch' : 'CATCH',
+    'new' : 'NEW',
+    'void' : 'VOID',
+    'function' : 'FUNCTION',
+    'const' : 'CONST',
+    'import' : 'IMPORT',
+    'default' : 'DEFAULT',
+    'continue' : 'CONTINUE',
+    'do' : 'DO',
+    'delete' : 'DELETE',
+    'extends' : 'EXTENDS',
+    'case' : 'CASE',
+    'super' : 'SUPER',
+    'export' : 'EXPORT',
+    'await' : 'AWAT',
+    'break' : 'BREAK',
+    'debugger' : 'DEBUGGER'
+
+}
