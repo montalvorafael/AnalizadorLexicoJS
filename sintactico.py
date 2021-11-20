8e15156fa1d35e02b2e6dfefd5f2691ee3f999a2
willymateo <wjmateo@espol.edu.ec>
2021-11-20 12:51:22 -0500

    Agregación test con algoritmo

--- sintactico.py
+++ sintactico.py
@@ -143,12 +143,11 @@ def p_empty(p):
 # Un parámetro.
 def p_funcion_unparametro(p):
     'funcion : FUNCTION VARIABLE IZQPAREN VARIABLE DERPAREN IZQLLAVE js DERLLAVE'
-#function cuadrado (numero){ numero* numero };
 
 # Sin parámetros.
 def p_funcion_sinparametro(p):
     'funcion : FUNCTION VARIABLE IZQPAREN DERPAREN IZQLLAVE js DERLLAVE'
-# function saludo (){ exp='hola' }
+
 # =========================================================================================
 
 
@@ -163,9 +162,6 @@ def p_if(p):
 def p_if_else(p):
     '''if : IF IZQPAREN comparacion DERPAREN IZQLLAVE js DERLLAVE ELSE IZQLLAVE js DERLLAVE'''
 
-#if (num > num2) { minimo =num2} if (num > num3) { minimo=num3}
-#if (num2 > num) { num2=num2-num} if (num > num3) { num =num-num3} else {num=100}
-
 # Switch ==================================================================================
 def p_switch(p):
     '''switch : SWITCH IZQPAREN VARIABLE DERPAREN IZQLLAVE ncasos DEFAULT DOSPUNTOS js DERLLAVE'''
@@ -178,11 +174,10 @@ def p_ncasos(p):
     '''ncasos : casos
     | casos ncasos
     '''
-#switch (expr) { case 1: exp=12; break; case 2: exp2='hola'; break; case 3: default: x=true; }
 
 # While ===================================================================================
 def p_while(p):
-    '''while : WHILE IZQPAREN comparacion DERPAREN IZQLLAVE expresion DERLLAVE'''
+    '''while : WHILE IZQPAREN comparacion DERPAREN IZQLLAVE js DERLLAVE'''
 
 # =========================================================================================
 
@@ -213,12 +208,6 @@ def p_arreglo(p):
     | LET VARIABLE IGUAL NEW ARRAY IZQPAREN lista DERPAREN
     | LET VARIABLE IGUAL IZQCORCHETE DERCORCHETE '''
 
-    # var nombres = new Array();
-    # var nombres = new Array(5);
-    # var nombres = new Array("Ana","Diana","Tom");
-    # var nombres = ["Ana","Diana","Tom"];
-    # var nombres = []; // arreglo vacío
-
 def p_lista(p):
     ''' lista : lista COMA lista
     | tipos_datos'''
@@ -227,13 +216,10 @@ def p_lista(p):
 def p_pop(p):
     '''pop : VAR VARIABLE IGUAL VARIABLE PUNTO POP IZQPAREN DERPAREN FINALDELINEA
     | VAR VARIABLE IGUAL VARIABLE PUNTO POP IZQPAREN DERPAREN'''
-#var eliminado = num.pop();
 
 def p_push(p):
     '''push : VAR VARIABLE IGUAL VARIABLE PUNTO PUSH IZQPAREN lista DERPAREN FINALDELINEA
     | VAR VARIABLE IGUAL VARIABLE PUNTO PUSH IZQPAREN lista DERPAREN'''
-#var mascolores = colores.push("verde", "celeste");
-
 
 # Map =====================================================================================
 def p_map(p):
@@ -241,13 +227,9 @@ def p_map(p):
     | VAR VARIABLE IGUAL NEW MAP IZQPAREN DERPAREN final_linea
     | CONST VARIABLE IGUAL NEW MAP IZQPAREN DERPAREN final_linea'''
 
-# let expr = new Map();
-# const expr2 = new Map()
-
 # Métodos.
 def p_mapSet(p):
     '''mapSet : MAPLOWER PUNTO SET IZQPAREN tipos_datos COMA tipos_datos DERPAREN final_linea'''
-# map.set('1', 2);
 
 # =========================================================================================
 
@@ -265,13 +247,104 @@ def p_error(p):
 # =========================================================================================
 parser_js = yacc.yacc(start=start_rule)
 
-while True:
-    try:
-        text = input('calc > ')
-    except EOFError:
-        break
-    if not text: continue
-    result = parser_js.parse(text, lexer= lexer_js)
+# Test ====================================================================================
+# Ingreso por teclado.
+# while True:
+# try:
+# text = input('calc > ')
+# except EOFError:
+# break
+# if not text: continue
+# result = parser_js.parse(text, lexer= lexer_js)
+# print(result)
+
+# Algoritmo de prueba
+data = [
+    # Declaración y asignacion.
+    'var x;',
+    'var _nueva = 0xfff;',
+    'var pruebaE = 23e-8;',
+    'var NuevaVariable = true;',
+    'var $_$0 = "grupo 8";',
+    'var sym1 = Symbol();',
+    'var sym2 = Symbol("foo");',
+    'let t1posdat0s = 789.8;',
+    'let $otranueva = false;',
+    'let pruebasNega = -789n;',
+
+    # Estructuras de datos.
+    'arreglo[0] = -893.2;',
+    'let array = new Array()',
+    'var nombres = new Array();',
+    'var nombres = new Array(5);',
+    'var nombres = new Array("Ana","Diana","Tom");',
+    'var nombres = ["Ana","Diana","Tom"];',
+    'var nombres = []; // arreglo vacío',
+    'var eliminado = num.pop();',
+    'var mascolores = colores.push("verde", "celeste");',
+    'let expr = new Map();',
+    'const expr2 = new Map()',
+    'map.set("1", 2);',
+
+    # Estructuras de control.
+    '''
+    if (9318471394913n > -200) {
+        var varNum = 0b010010011110;
+    }
+    ''',
+    '''
+        if (num > num2) {
+            minimo =num2
+        } if (num > num3) {
+            minimo=num3
+        }
+    ''',
+    '''
+        if (num2 > num) {
+            num2=num2-num
+        } if (num > num3) {
+            num =num-num3
+        } else {
+            num=100
+        }
+    ''',
+    '''
+    while (n < 3) {
+        x += n;
+    }
+    ''',
+    '''
+    switch (expr) {
+        case 1:
+            exp=12;
+            break;
+        case 2:
+            exp2='hola';
+            break;
+        case 3:
+        default:
+            x=true;
+    }
+    ''',
+
+    # Funciones
+    '''
+    function cuadrado(numero) {
+        numero* numero;
+    }
+    ''',
+    '''
+    function saludo(){
+        exp='hola'
+    }
+    ''',
+]
+
+for instruccion in data:
+    print(instruccion)
+    result = parser_js.parse(instruccion, lexer= lexer_js)
     print(result)
+    print()
+
 # =========================================================================================
 
