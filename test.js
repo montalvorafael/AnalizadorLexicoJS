00c2ea2a306e41359282330026a88ecb4e6c4b0b
Rafael Montalvo <rafamont@espol.edu.ec>
2021-11-30 20:16:57 -0500

    correcion algoritmo

--- test.js
+++ test.js
@@ -1,14 +1,11 @@
-//Declaración y asignacion.
+
 var num;
 var NULL;
 num2 = true
-10/"hola"
 num = num-num3
 num=100
 5*2/1+1
 1*1+1
-4+5+5-true
-4+5+5+"ok"
 var _nueva = 0xfff;
 var pruebaE = 23e-8;
 var NuevaVariable = true;
@@ -19,14 +16,13 @@ let t1posdat0s = 789.8;
 let $otranueva = false;
 let pruebasNega = -789n;
 
-//Operadores logicos.
+
 n>0 && n <5
 num>5 || num ==10
 num>5 || num ==10 || num <= 0
 num>5 || num ==10 && num <= 0
-num>5 || num && 10
 
-//Estructuras de datos.
+
 arreglo[0] = -893.2;
 let array = new Array()
 var nombres = new Array();
@@ -52,70 +48,27 @@ mySet.add("hola mundo");
 mySet.add(5.45);
 mySet.add(3);
 
-//Estructuras de control.
-if (9318471394913n > -200) {
-	var varNum = 0b010010011110;
-}
 
-if (num > num2) {
-	minimo =num2
-} if (num > num3) {
-	minimo=num3
-}
+if (9318471394913n > -200) {var varNum = 0b010010011110;}
 
-if (num2 > num) {
-	num2=num2-num
-} if (num > num3) {
-	num =num-num3
-} else {
-	num=100
-}
+if (num > num2) {minimo =num2} if (num > num3) {minimo=num3}
 
-if (num2 > num || num2 == 10) {
-	num2=num2-num
-} if (num > num3) {
-	num =num-num3
-} else {
-	num=100
-}
+if (num2 > num) {num2=num2-num} if (num > num3) {num =num-num3} else {num=100}
 
-while (n < 3 ) {
-	x = n;
-}
+if (num2 > num || num2 == 10) {num2=num2-num} if (num > num3) {num =num-num3} else {num=100}
 
-while (n < 3 && n>0) {
-	x = n;
-}
+while (n < 3 ) {x = n;}
 
-while (menor <= 3) {
-	x = n;
-	let s = new Array();
-}
+while (n < 3 && n>0) {x = n;}
 
-switch (expr) {
-	case 1:
-		exp=12;
-		break;
-	case 2:
-		exp2='hola';
-		break;
-	case 3:
-	default:
-		x=true;
-}
+while (menor <= 3) {x = n; let s = new Array();}
 
-//Funciones
-function saludo(){
-	exp='hola';
-}
+switch (expr) {case 1:exp=12;break; case 2:exp2='hola';break; case 3: default:x=true;}
 
-function cuadrado(numero) {
-	let sopa de letras= "fndklañfdiafhwbnlewq"
-	numero* numero;
-}
 
-function newFunction(a, b, c) {
-	let suma = a + b + c;
-	return 0;
-}
+function saludo(){exp='hola';}
+
+function cuadrado(numero) {let sopa de letras= "fndklañfdiafhwbnlewq"numero* numero;}
+
+function newFunction(a, b, c) {let suma = a + b + c;return 0;}
 
